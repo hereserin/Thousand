0abb03d87659eb0d6b1ed80a72bb6adab2ca9c98
Erin Marshall <erin.e.mars@gmail.com>
2019-03-21 13:31:53 -0400

    Link back and frontend

---
+++ frontend/components/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Thousand</h1>
+      <h2>This is my app.</h2>
+    </div>
+  );
+};
+
+export default App;
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import entitiesReducer from "./entities_reducer";
-import uiReducer from "./ui_reducer";
 
 const rootReducer = combineReducers({
-  entities: entitiesReducer,
-  ui: uiReducer
+  entities: entitiesReducer
 });
 
 export default rootReducer;
